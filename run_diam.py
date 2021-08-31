aa9c8273f94133556cc3b75854657b81dcc2b071
Gonzalo Zigarán <gjzigaran@gmail.com>
2021-08-31 16:11:43 -0300

    cambios y ejecucion de run_diam

--- run_diam.py
+++ run_diam.py
@@ -4,6 +4,7 @@
 from datetime import datetime
 from functools import reduce
 import logging
+from itertools import combinations, product
 
 from folpy.utils.parser.parser import Parser
 
@@ -13,10 +14,64 @@ from atomic import is_global_indecomposable_atomics
 # from minion_globalspectrum import is_global_spectrum_minion_relation_n
 
 
+
+def maximal_substructures(
+        model,
+        supermodel=None,
+        filter_isos=True,
+        filter_subdirect=False):
+    if not supermodel:
+        supermodel = model
+    if filter_subdirect:
+        assert len(model.factors) > 1
+    universe = model.universe.copy()
+    result = []
+    result_compl = []
+    for i in range(1, len(model)):
+        has_subs_of_this_len = False
+        for subset in combinations(universe, i):
+            subset = set(subset)
+            if any(x.issubset(subset) for x in result_compl):
+                continue
+            has_subs_of_this_len = True
+            possible_subuniverse = [x for x in universe if x not in subset]
+            if is_subuniverse(possible_subuniverse, model):
+                substructure = supermodel.restrict(possible_subuniverse)
+                result_compl.append(subset)
+                are_isos = any(substructure.is_isomorphic(x) for x in result)
+                if filter_isos and are_isos:
+                    continue
+                result.append(substructure)
+                yield substructure
+                yield from maximal_substructures(
+                                substructure,
+                                supermodel=supermodel,
+                                filter_isos=True,
+                                filter_subdirect=False)
+        if not has_subs_of_this_len:
+            break
+    return result
+
+def is_subuniverse(possible_subuniverse, model):
+    for x in product(possible_subuniverse, repeat=2):
+        if model.join(*x) not in possible_subuniverse:
+            return False
+        if model.meet(*x) not in possible_subuniverse:
+            return False
+    return True
+
+
+
+
+
+
 def check_isos(sub, subs):
     for s in subs:
+        logging.debug(s.universe)
         if sub.is_isomorphic_graph(s):
+            logging.debug("iso")
             return True
+    logging.debug("no iso")
     return False
 
 
@@ -26,16 +81,21 @@ def gen_subdirect_sublattices(lattices, verbose=False):
     subs = []
     j = 0
     i = 0
-    for emb, sub in L.substructures():
+    for sub in maximal_substructures(L):
         j = j + 1
-        if j % 500 == 0:
-            logging.debug('%s (Time: %s)', j, (datetime.now() - start_time))
-        if sub.is_subdirect() and not sub.is_isomorphic(L):
+        if j % 1 == 0:
+            logging.debug('Substructure Nº: %s (Time: %s)', j, (datetime.now() - start_time))
+        is_subdirect = sub.is_subdirect()
+        logging.debug("subdirect: %s", is_subdirect)
+        is_iso = sub.is_isomorphic_graph(L)
+        logging.debug("iso: %s", is_iso)
+        if is_subdirect and not is_iso:
+            logging.debug("cumple")
             if not check_isos(sub, subs):
                 subs.append(sub)
                 i = i + 1
-                if i % 10 == 0:
-                    logging.debug(i)
+                if i % 1 == 0:
+                    logging.debug('Subdirect Product Nº: %s', i)
     if verbose:
         logging.info("Cantidad de sublattices subdirectos: %s", len(subs))
         logging.info("Tiempo sublattices: %s", (datetime.now() - start_time))
@@ -45,7 +105,7 @@ def gen_subdirect_sublattices(lattices, verbose=False):
 
 if __name__ == "__main__":
     logging.basicConfig(
-        filename='run_diam.log',
+        filename='out_run_diam2.log',
         format='%(asctime)s - %(levelname)s: %(message)s',
         level=logging.DEBUG
         )
@@ -90,6 +150,8 @@ if __name__ == "__main__":
                                                   congruence_lattice=con_lat)))
         i += 1
 
+    logging.info("FIN")
+
     # continuos = subs_Diam2[4].continous()
     # con = continuos[0].principal_congruence(1, 5)
     # print(continuos)
@@ -97,3 +159,5 @@ if __name__ == "__main__":
     # con_lat = subs_Diam2[4].congruence_lattice()
     # con_lat.draw()
     # is_global_indecomposable_atomics(con_lat)
+
+
